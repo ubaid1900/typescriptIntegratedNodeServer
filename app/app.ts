3f96183d748f5f43c21ba13271afdf6991cc5481
Ubaid Mohammed <ubaid1900@gmail.com>
2020-06-21 15:08:07 +0530

    initial

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,23 @@
 // lib/app.ts
 import express = require('express');
+var multer = require('multer');
+var uploader = multer({ dest: 'uploads/' });
+var cors = require('cors');
+
 
 // Create a new express application instance
 const app: express.Application = express();
-
+app.use(cors())
 app.get('/', function (req, res) {
-  res.send('Hello World! Keep on going');
+  res.send({ responseText: 'Hello World! Keep on going' });
+});
+
+app.post('/upload', uploader.array('files[]'), function (req, res) {
+  const fl: FileList = req.files;
+  res.send({
+    responseText: `received ${req.files.length} file(s) to upload`,
+    serialized: `${fl[0].filename}, ${fl[0].originalfileName}`
+  });
 });
 
 app.listen(3000, function () {

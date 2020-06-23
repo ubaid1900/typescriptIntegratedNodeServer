ac86f8b71fbb901d5a3691a45ff91e02895ce16d
Ubaid Mohammed <ubaid1900@gmail.com>
2020-06-23 15:24:18 +0530

    add get

--- app/app.ts
+++ app/app.ts
@@ -11,14 +11,29 @@ app.use(cors())
 app.get('/', function (req, res) {
   res.send({ responseText: 'Hello World! Keep on going' });
 });
-
-app.post('/upload', uploader.array('files[]'), function (req, res) {
-  const fl: FileList = req.files;
-  res.send({
-    responseText: `received ${req.files.length} file(s) to upload`,
-    serialized: `${fl[0].filename}, ${fl[0].originalfileName}`
-  });
+var times = [
+  { id: 1, name: 'Fajr', preferredTime: '5:00 AM' },
+  { id: 2, name: 'Zuhr', preferredTime: '1:30 PM' },
+  { id: 3, name: 'Asr', preferredTime: '5:15 PM' },
+  { id: 4, name: 'Maghrib', preferredTime: '7:00 PM' },
+  { id: 5, name: 'Isha', preferredTime: '8:15 PM' }];
+app.get('/prayTimes', function (req, res) {
+  res.send({ times: times });
 });
+app.get('/prayTimes/byname/:prayerName', function (req, res) {
+  res.send({ times: times.find(f => f.name.toLowerCase() === req.params.prayerName.toLowerCase()) });
+});
+app.get('/prayTimes/:prayerId', function (req, res) {
+  res.send({ times: times.find(f => f.id === +req.params.prayerId) });
+});
+
+// app.post('/upload', uploader.array('files[]'), function (req, res) {
+//   const fl: FileList = req.files;
+//   res.send({
+//     responseText: `received ${req.files.length} file(s) to upload`,
+//     serialized: `${fl[0].filename}, ${fl[0].originalfileName}`
+//   });
+// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
